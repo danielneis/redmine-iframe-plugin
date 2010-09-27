fe57ca993b0c7ede351f652d45954ae2d9762d0f
Daniel Neis Araujo <daniel@npd.ufsc.br>
2010-09-27 15:12:15 -0300

    Send project_id as parameter to iframe

--- app/controllers/iframe_controller.rb
+++ app/controllers/iframe_controller.rb
@@ -2,6 +2,7 @@ class IframeController < ApplicationController
   unloadable
 
   def index
+      @project_id = params['project_id']
       @project = Project.find(params['project_id'])
       @source_url = Setting.plugin_redmine_iframe['source_url']
       @title = Setting.plugin_redmine_iframe['title']
