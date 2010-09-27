3fd940b512958fd0f1052b526171d4cfbb0c5992
Daniel Neis Araujo <daniel@npd.ufsc.br>
2010-09-27 15:03:53 -0300

    Correcting the way to get project id

--- app/controllers/iframe_controller.rb
+++ app/controllers/iframe_controller.rb
@@ -2,7 +2,7 @@ class IframeController < ApplicationController
   unloadable
 
   def index
-      @project = Project.find(params[:id])
+      @project = Project.find(params['project_id'])
       @source_url = Setting.plugin_redmine_iframe['source_url']
       @title = Setting.plugin_redmine_iframe['title']
   end
